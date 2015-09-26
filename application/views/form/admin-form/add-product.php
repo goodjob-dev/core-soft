6fe98e693df9177a3b825143b2d051dbc0920e73
arthurkhlghatyan <arthur.khlghatyan@gmail.com>
2015-09-26 11:00:24 +0400

    Editing product form

--- application/views/form/admin-form/add-product.php
+++ application/views/form/admin-form/add-product.php
@@ -1,13 +1,13 @@
 <div class="container main-content">
 	<div class="row">
-		<h1 class="none-padding page-title col-xs-3"><?php echo $title;?></h1>
+		<h1 class="page-title col-xs-3"><?php echo $title;?></h1>
 	</div>
 	<div class="row">
 
 		<div class="add-product-layout col-xs-8">
 				
 
-			<form method="post" action="" enctype="multipart/form-data">
+			<form method="post" action="" class="form-horizontal" enctype="multipart/form-data">
 
 				
 
@@ -16,8 +16,7 @@
 					<div class="col-lg-9">
 						<input type="text" name="title" id="title" class="form-control" value="">
 					</div>
-					<div class="clear"></div>
-				</div><br>
+				</div>
 
 				<div class="form-group">
 					<label for="title" class="col-lg-3 control-label">Category *</label>
@@ -30,8 +29,7 @@
 							<option>5</option>
 						</select>
 					</div>
-					<div class="clear"></div>
-				</div><br>
+				</div>
 
 				
 				<div class="form-group">
@@ -39,30 +37,27 @@
 					<div class="col-lg-9">
 						<input type="number" name="regular_price" id="regular_price" class="form-control" value="">
 					</div>
-					<div class="clear"></div>
-				</div><br>
+				</div>
 
 				<div class="form-group">
 					<label for="sale_price" class="col-lg-3 control-label">Sale Price</label>
 					<div class="col-lg-9">
 						<input type="number" name="sale_price" id="sale_price" class="form-control" value="">
 					</div>
-					<div class="clear"></div>
-				</div><br>
+				</div>
 
 				<div class="form-group">
 					<label for="product_image" class="col-lg-3 control-label">Product image</label>
 					<div class="col-lg-9">
 						<input type="file" name="product_image" id="product_image" class="form-control" value="">
 					</div>
-					<div class="clear"></div>
-				</div><br>
+				</div>
 				
 			
 				<div class="form-group">
 					<div class="col-lg-12">
-						<input type="submit" name="submit" class="btn btn-success" style="margin-bottom:20px;">
-						<input type="button" name="submit" class="btn btn-danger" style="margin-bottom:20px;" value="Cancel">
+						<input type="submit" name="submit" class="btn btn-success" value="Add">
+						<input type="button" name="submit" class="btn btn-danger" value="Cancel">
 					</div>
 				</div>
 
